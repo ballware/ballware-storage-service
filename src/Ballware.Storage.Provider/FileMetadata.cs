834a6e0412f613ae46593a90cddd9f89a7d3797d
Frank Ballmeyer <frank@ballmeyer.com>
2024-12-27 15:19:06 +0100

    Fixed wrong serialization of file list

--- src/Ballware.Storage.Provider/FileMetadata.cs
+++ src/Ballware.Storage.Provider/FileMetadata.cs
@@ -1,6 +1,11 @@
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
 namespace Ballware.Storage.Provider;
 
 public sealed class FileMetadata
 {
+    [JsonProperty("Name")]
+    [JsonPropertyName("Name")]
     public required string Filename { get; set; }
 }
