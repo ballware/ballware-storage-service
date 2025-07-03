106836d6f458595dbbb4a72a88c3638bf2b48cd1
Frank Ballmeyer <frank@ballmeyer.com>
2025-07-03 21:27:24 +0200

    Reduced duplicated code in persistables #1

--- src/Ballware.Storage.Data/Persistables/Attachment.cs
+++ src/Ballware.Storage.Data/Persistables/Attachment.cs
@@ -1,40 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace Ballware.Storage.Data.Persistables;
 
 [Table("attachment")]
-public class Attachment : IEntity, IAuditable, ITenantable
+public class Attachment : Blob
 {
-    [JsonIgnore]
-    [Column("id")]
-    public virtual long? Id { get; set; }
-
-    [JsonPropertyName(nameof(Id))]
-    [Column("uuid")]
-    public virtual Guid Uuid { get; set; }
-
-    [JsonIgnore]
-    [Column("tenant_id")]
-    public virtual Guid TenantId { get; set; }
-    
-    [JsonIgnore]
-    [Column("creator_id")]
-    public virtual Guid? CreatorId { get; set; }
-
-    [JsonIgnore]
-    [Column("create_stamp")]
-    public virtual DateTime? CreateStamp { get; set; }
-
-    [JsonIgnore]
-    [Column("last_changer_id")]
-    public virtual Guid? LastChangerId { get; set; }
-
-    [JsonIgnore]
-    [Column("last_change_stamp")]
-    public virtual DateTime? LastChangeStamp { get; set; }
-    
     [Required]
     [Column("entity")]
     public string? Entity { get; set; }
@@ -42,20 +13,4 @@ public class Attachment : IEntity, IAuditable, ITenantable
     [Required]
     [Column("owner_id")]
     public Guid OwnerId { get; set; }
-    
-    [Required]
-    [Column("file_name")]
-    public string? FileName { get; set; }
-    
-    [Required]
-    [Column("content_type")]
-    public string? ContentType { get; set; }
-    
-    [Required]
-    [Column("file_size")]
-    public long FileSize { get; set; }
-    
-    [Required]
-    [Column("storage_path")]
-    public string? StoragePath { get; set; }
 }
